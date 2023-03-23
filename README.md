6b9357d8cf88c1cc42397bdf2990e174edda5b2b
Antonio Cheong <acheong@student.dalat.org>
2023-03-23 23:22:53 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# EdgeGPT-Proxy
+
+## Installation 
+
+`go install github.com/acheong08/EdgeGPT-Proxy@latest`
+
+### Running
+`EdgeGPT-Proxy`
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	http "github.com/bogdanfinn/fhttp"
+	tls_client "github.com/bogdanfinn/tls-client"
+
+	"github.com/fvbock/endless"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	jar     = tls_client.NewCookieJar()
+	options = []tls_client.HttpClientOption{
+		tls_client.WithTimeoutSeconds(360),
+		tls_client.WithClientProfile(tls_client.Chrome_110),
+		tls_client.WithNotFollowRedirects(),
+		tls_client.WithCookieJar(jar), // create cookieJar instance and pass it as argument
+	}
+	client, _ = tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
+)
+
+func main() {
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "8080"
+	}
+	handler := gin.Default()
+	handler.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	handler.GET("/edgesvc/turing/conversation/create", proxy)
+
+	endless.ListenAndServe(os.Getenv("HOST")+":"+PORT, handler)
+}
+
+func proxy(c *gin.Context) {
+
+	var url string
+	var err error
+	var request_method string
+	var request *http.Request
+	var response *http.Response
+
+	url = "https://edgeservices.bing.com/edgesvc/turing/conversation/create"
+	request_method = c.Request.Method
+
+	request, err = http.NewRequest(request_method, url, c.Request.Body)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Copy headers
+	for key, value := range c.Request.Header {
+		request.Header.Set(key, value[0])
+	}
+
+	response, err = client.Do(request)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	defer response.Body.Close()
+	c.Header("Content-Type", response.Header.Get("Content-Type"))
+	// Get status code
+	c.Status(response.StatusCode)
+	c.Stream(func(w io.Writer) bool {
+		// Write data to client
+		io.Copy(w, response.Body)
+		return false
+	})
+
+}
